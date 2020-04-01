aa093c9afc9ce3fc9642b46017c2c15401d7c908
Vicky <vyue013@gmail.com>
2020-04-01 11:15:16 -0700

    drafted table with initial dummy data

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import VaccineTable from './VaccineTable';
+
 function Main() {
   return (
     <main className="container">
@@ -8,6 +10,7 @@ function Main() {
           <div className="columns is-9-desktop is-gapless main">
             <div className="column">
               <h1 className="title">Ncov-19 Vaccine Dashboard</h1>
+              <VaccineTable />
             </div>
             <div className="column">
               <h2 className="title">Right Component</h2>
---
+++ src/pages/VaccineTable.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+
+import Table from "./Table";
+
+function VaccineTable() {
+
+    const [tableData] = useState([
+        { Sponsors: "BioNTech SE and Pfizer Inc.", Country: "USA", Drug: "BNT162", Phase: "Preclinical", Type: "Vaccine" },
+        { Sponsors: "Gilead Sciences Inc.", Country: "USA", Drug: "remdesivir", Phase: "Phase 2", Type: "Treatment" },
+        { Sponsors: "GlaxoSmithKline", Country: "USA", Drug: "AS03 Adjuvant System", Phase: "None", Type: "Adjuvant platform for vaccines" },
+        { Sponsors: "Heat Biologics Inc.", Country: "USA", Drug: "None", Phase: "Preclinical", Type: "Vaccine" },
+        { Sponsors: "Inovio Pharmaceuticals Inc.", Country: "USA", Drug: "INO-4800", Phase: "Preclinical", Type: "DNA-based vaccine" },
+    ]);
+
+    return (
+        <div>
+            <h2>COVID-19 Trials</h2>
+            <h2>Vaccines</h2>
+            <h2>Treatments</h2>
+            <h2>Alternatives</h2>
+            <Table data={tableData} />
+        </div>
+    );
+}
+
+export default VaccineTable;
