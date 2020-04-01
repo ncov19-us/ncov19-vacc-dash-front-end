bb2e997f4337740626354652922a116881790c52
Jason Hemann <43769789+JasonRHemann@users.noreply.github.com>
2020-04-01 17:48:13 -0500

    Map not currently working

--- src/App.js
+++ src/App.js
@@ -3,15 +3,16 @@ import React from "react";
 import Header from "./pages/Header";
 import Footer from "./pages/Footer";
 import Main from "./pages/Main";
+import WorldMap from "./components/WorldMap";
 
 import "./App.css";
 
 export default function App() {
-	return (
-		<div className="App">
-			<Header />
-			<Main />
-			<Footer />
-		</div>
-	);
+  return (
+    <div className="App">
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
 }
