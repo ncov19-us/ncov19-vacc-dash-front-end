f7ca02f5d2e52ee7d3882b545ee1870565b24b13
ljh-c <57099181+ljh-c@users.noreply.github.com>
2020-04-01 23:22:16 -0700

    chore: remove index.css

--- src/index.js
+++ src/index.js
@@ -4,8 +4,9 @@ import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
-import "./index.css";
-import "bulma/css/bulma.css";
+// import "./index.css";
+// import "bulma/css/bulma.css";
+import "semantic-ui-css/semantic.min.css";
 
 ReactDOM.render(
 	<React.StrictMode>
