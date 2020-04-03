b2d837279a67d47877c8b5ee441b0588226b69d1
Jason Hemann <43769789+JasonRHemann@users.noreply.github.com>
2020-04-02 20:38:13 -0500

    testing update file

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -6,16 +6,18 @@ import { treatments } from "../data/treatment-list";
 import data from "../data/map-data";
 
 function Main() {
+  const [treatmentsList, setTreatmentsList] = useState(treatments);
   const [filteredCountry, setFilteredCountry] = useState("Global");
 
   return (
     <>
-      <DashTopper country={filteredCountry} />
       <main className="ui centered grid">
         <div className="twelve wide column">
           <div className="ui stackable grid">
             <div className="two column row">
               <div className="sixteen wide tablet eight wide computer column">
+                <h1 className="title">{filteredCountry} Dashboard</h1>
+                <DashTopper />
                 <VaccineTable />
               </div>
               <div className="sixteen wide tablet eight wide computer column">
