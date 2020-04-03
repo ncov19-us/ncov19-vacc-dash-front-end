755b16ada01f5bba0f8db4a7b0be973d40401acd
ljh-c <57099181+ljh-c@users.noreply.github.com>
2020-04-03 15:50:39 -0700

    chore: populate table with dummy data

--- src/pages/VaccineTable.js
+++ src/pages/VaccineTable.js
@@ -15,11 +15,48 @@ function VaccineTable() {
 		getTrials();
 	}, []);
 
+	const tableData = [
+		{
+			Sponsors: "BioNTech SE and Pfizer Inc.",
+			Country: "USA",
+			Drug: "BNT162",
+			Phase: "Preclinical",
+			Type: "Vaccine",
+		},
+		{
+			Sponsors: "Gilead Sciences Inc.",
+			Country: "China",
+			Drug: "remdesivir",
+			Phase: "Phase 2",
+			Type: "Treatment",
+		},
+		{
+			Sponsors: "GlaxoSmithKline",
+			Country: "USA",
+			Drug: "AS03 Adjuvant System",
+			Phase: "None",
+			Type: "Adjuvant platform for vaccines",
+		},
+		{
+			Sponsors: "Heat Biologics Inc.",
+			Country: "USA",
+			Drug: "None",
+			Phase: "Preclinical",
+			Type: "Vaccine",
+		},
+		{
+			Sponsors: "Inovio Pharmaceuticals Inc.",
+			Country: "USA",
+			Drug: "INO-4800",
+			Phase: "Preclinical",
+			Type: "DNA-based vaccine",
+		},
+	];
+
 	return (
 		<div>
 			<TrialMenu />
-			{/* {data={tableData}} */}
-			{/* <Table /> */}
+			<Table data={tableData} />
 		</div>
 	);
 }
