908eecdb1af6a38e467c792ecbefdce38ff233a0
ljh-c <57099181+ljh-c@users.noreply.github.com>
2020-04-06 21:30:53 -0700

    feat: :sparkles: add functionality to click on row and open study link

--- src/pages/Table.js
+++ src/pages/Table.js
@@ -1,60 +1,105 @@
-import React from "react";
+import React from 'react';
 
 export default class Table extends React.Component {
-	// props.data
-	constructor(props) {
-		super(props);
-		this.getHeader = this.getHeader.bind(this);
-		this.getRowsData = this.getRowsData.bind(this);
-		this.getColumns = this.getColumns.bind(this);
-	}
+  // props.data
+  constructor(props) {
+    super(props);
+    this.getHeader = this.getHeader.bind(this);
+    this.getRowsData = this.getRowsData.bind(this);
+    this.getColumns = this.getColumns.bind(this);
+  }
 
-	getColumns = function () {
-		return ["Sponsors", "Country", "Intervention", "Phase", "Type"];
-	};
+  getColumns = function () {
+    return ['Sponsors', 'Country', 'Intervention', 'Phase', 'Type'];
+  };
 
-	getHeader = function () {
-		var keys = this.getColumns();
-		return keys.map((key) => {
-			return <th key={key}>{key}</th>;
-		});
-	};
+  getHeader = function () {
+    var keys = this.getColumns();
+    return keys.map((key) => {
+      return <th key={key}>{key}</th>;
+    });
+  };
 
-	getRowsData = function () {
-		var items = this.props.data;
-		return items.map((row, index) => {
-			return (
-				<tr key={index}>
-					<RenderRow key={index} data={row} />
-				</tr>
-			);
-		});
-	};
+  getRowsData = function () {
+    var items = this.props.data;
+    return items.map((row, index) => {
+      return (
+        <tr key={index} className="record">
+          <RenderRow key={index} data={row} />
+        </tr>
+      );
+    });
+  };
 
-	render() {
-		return (
-			<div className="table-wrapper">
-				<table className="table is-hoverable">
-					<thead>
-						<tr>{this.getHeader()}</tr>
-					</thead>
-					<tbody>{this.getRowsData()}</tbody>
-				</table>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className="table-wrapper">
+        <table className="table">
+          <thead>
+            <tr>{this.getHeader()}</tr>
+          </thead>
+          <tbody>{this.getRowsData()}</tbody>
+        </table>
+      </div>
+    );
+  }
 }
 
 const RenderRow = (props) => {
-	// props.data
+  // props.data
 
-	return (
-		<>
-			<td>{props.data.sponsors}</td>
-			<td>{props.data.countries}</td>
-			<td>{props.data.intervention}</td>
-			<td>{props.data.phase}</td>
-			<td>{props.data.intervention_type}</td>
-		</>
-	);
+  return (
+    <>
+      <td>
+        <a
+          href={props.data.data_source}
+          target="_blank"
+					rel="noreferrer noopener"
+					// style={{ color: 'white' }}
+        >
+          {props.data.sponsors}
+        </a>
+      </td>
+      <td>
+        <a
+          href={props.data.data_source}
+          target="_blank"
+					rel="noreferrer noopener"
+					// style={{ color: 'white' }}
+        >
+          {props.data.countries}
+        </a>
+      </td>
+      <td>
+        <a
+          href={props.data.data_source}
+          target="_blank"
+					rel="noreferrer noopener"
+					// style={{ color: 'white' }}
+        >
+          {props.data.intervention}
+        </a>
+      </td>
+      <td>
+        <a
+          href={props.data.data_source}
+          target="_blank"
+					rel="noreferrer noopener"
+					// style={{ color: 'white' }}
+        >
+          {props.data.phase}
+        </a>
+      </td>
+      <td>
+        <a
+          href={props.data.data_source}
+          target="_blank"
+					rel="noreferrer noopener"
+					// style={{ color: 'white' }}
+        >
+          {props.data.intervention_type}
+        </a>
+      </td>
+    </>
+  );
 };
