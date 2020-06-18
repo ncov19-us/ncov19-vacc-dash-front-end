8b86c7926d86a0a8e4b70b0108b832bae78cc585
Alex <hello@alex-ak.com>
2020-06-17 19:41:44 -0700

    fix: adjust conditional logic for get request to get proper counts.

--- src/pages/VaccineTable.js
+++ src/pages/VaccineTable.js
@@ -11,10 +11,11 @@ GOAL:
 */
 
 function VaccineTable({ filterInfo, dispatch }) {
-	const { getTrials, getDashCardsByCountryAndType, getDashCardsGlobal, table, isLoading, count } = useContext(TableContext);
+	const { getTrials, getDashCardsByCountryAndType, getDashCardsGlobal, getDashCardsByTypeGlobal, table, isLoading, count } = useContext(TableContext);
 
 	const { page, country, type } = filterInfo;
 
+	// fetch table data and card totals
 	useEffect(() => {
 		let apiUrl = `api/trials?limit=7&page=${page}`;
 
@@ -27,12 +28,19 @@ function VaccineTable({ filterInfo, dispatch }) {
 		}
 
 		// fetch totals based on filters
-		if (country !== "world" || type !== "all") getDashCardsByCountryAndType(country, type);
 		if (country === "world" && type === "all") getDashCardsGlobal();
-		
+		if (country === 'world' && type !== "all") getDashCardsByTypeGlobal();
+		if (country !== "world") getDashCardsByCountryAndType(country, type);
+
 		// fetch dynamically filtered table results
 		getTrials(apiUrl);
 	}, [filterInfo, country, type]); // when filter info changes, fetch data
+
+
+	useEffect(() => {
+		console.log('test');
+	}, [country, type, getDashCardsGlobal])
+
 
 	return (
 		<div className="trial-padding">
